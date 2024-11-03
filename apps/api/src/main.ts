dc81e6ba026da3f4111b5a4de7ec548c47c2fd4a
Arroziqi <ahmadarroziqi@gmail.com>
2024-11-03 15:52:33 +0700

    create next and nest js project

---
+++ apps/api/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AppService } from './app.service';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const appService = app.get(AppService);
+
+  await app.listen(appService.getPort() ?? 8080);
+}
+bootstrap();
